e2697508f06b547aff9d1d25b4e9907158331da6
selinafehn <144441353+selinafehn@users.noreply.github.com>
2025-01-19 20:40:08 +0100

    logic for admin panel switch

--- components/header.vue
+++ components/header.vue
@@ -1,4 +1,36 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useStorage } from '@vueuse/core';
+
+// E-Mail des aktuellen Benutzers aus dem lokalen Speicher
+const userEmailStorage = useStorage('auth_email', '');
+const userEmail = userEmailStorage.value;
+
+// Zustand für die Adminrolle
+const isAdmin = ref(false);
+
+// Funktion, um die Adminrolle zu prüfen
+const checkAdminRole = async () => {
+  try {
+    console.log('Prüfe Adminrolle für:', userEmail);
+    const response = await fetch(
+        `https://b2c-backend-927d63ee0883.herokuapp.com/api/v1.0/user/role/admin/get?email=${userEmail}`
+    );
+
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Rolle');
+
+    isAdmin.value = await response.json(); // true oder false von der API
+    console.log('isAdmin:', isAdmin.value);
+  } catch (error) {
+    console.error('Rollenprüfung fehlgeschlagen:', error);
+    isAdmin.value = false; // Standard: Nicht-Admin
+  }
+};
+
+// API-Aufruf beim Laden der Komponente
+onMounted(() => {
+  checkAdminRole();
+});
 </script>
 
 
@@ -17,7 +49,15 @@
       <!-- Darkmode -->
         <Darkmode/>
       </div>
-    </div>
+
+  <Button
+      v-if="userRole === 'admin'"
+      @click="$router.push('/admin/dashboard')"
+      class="px-4 py-2 bg-blue-500 text-white rounded-md shadow hover:bg-blue-600"
+  >
+    Admin Dashboard
+  </Button>
+  </div>
 </template>
 
 
---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import Header from "~/components/header.vue";
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const items = computed(() => [
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    command: () => {
+      router.push('/')
+    }
+  }]);
+</script>
+
+<template>
+  <Header />
+  <MenuBar :model="items" class="text-gray-800" />
+</template>
+
+<style scoped>
+
+</style>
