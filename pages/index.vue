46e933e5a941a2588e67470e2a300453b81fcdac
selinafehn <144441353+selinafehn@users.noreply.github.com>
2025-02-07 23:10:34 +0100

    added chatbot

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,21 @@
  * Importiert die Header-Komponente für die Navigation und Seiteneinleitung.
  */
 import Header from "~/components/header.vue";
+/**
+ * Öffnet den chatbot
+ */
+const openChatbot = () => {
+  const width = 800;
+  const height = 600;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  window.open(
+      'https://nextchapter.azurewebsites.net',
+      '_blank',
+      `width=${width},height=${height},top=${top},left=${left},resizable=yes,scrollbars=yes`
+  );
+};
 </script>
 
 <template>
@@ -14,6 +29,15 @@ import Header from "~/components/header.vue";
   <BookDisplay />
   <!-- Footer-Komponente -->
   <Footer />
+  <!-- Container für den Button -->
+  <div class="fixed right-2 top-7/8">
+    <Button
+        label="🤖"
+        class="p-button-rounded p-button-success bg-gray-700"
+        :style="{ fontSize: '2rem', padding: '2rem', width: '2rem', height: '2rem' }"
+        @click="openChatbot"
+    />
+  </div>
 </template>
 
 <style scoped>
